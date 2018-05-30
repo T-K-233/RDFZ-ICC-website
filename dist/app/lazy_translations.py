5dc5ab107bbb0cf779c7752b132e31203fd0fbfd
-T.K.- <tk_dandelion@126.com>
2018-05-30 09:08:26 +0800

    -

---
+++ dist/app/lazy_translations.py
@@ -0,0 +1,25 @@
+from flask_babel import lazy_gettext
+
+lazy_gettext('about')
+lazy_gettext('about ICC')
+lazy_gettext('mission')
+lazy_gettext('faculty')
+lazy_gettext('matriculation')
+lazy_gettext('admission')
+
+lazy_gettext('academics')
+lazy_gettext('chinese curricula')
+lazy_gettext('Cambridge A Level Program')
+lazy_gettext('The Advanced Placement Program')
+lazy_gettext('International Baccalaureate')
+
+lazy_gettext('student')
+lazy_gettext('campus life')
+lazy_gettext('extra-curricular activity')
+
+lazy_gettext('resources')
+lazy_gettext('contacts')
+lazy_gettext('school calendar')
+lazy_gettext('campus map')
+lazy_gettext('location')
+lazy_gettext('for media')
